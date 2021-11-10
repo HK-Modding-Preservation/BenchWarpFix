2f1c1dd3a6e47dd8c733fd3d51b04b284f9d04a1
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2021-11-10 17:50:52 +0100

    Change coroutine to hook

--- BenchWarpFix.cs
+++ BenchWarpFix.cs
@@ -18,41 +18,37 @@ namespace BenchWarpFix
         {
             Instance = this;
 
-            GameManager.instance.StartCoroutine(FixMissingBench());
+            On.HeroController.FixedUpdate += this.OnHeroControllerFixedUpdate;
         }
 
-        private static IEnumerator FixMissingBench()
+        private void OnHeroControllerFixedUpdate(On.HeroController.orig_FixedUpdate orig, HeroController self)
         {
-            while (true)
+            orig(self);
+
+            var hero = HeroController.instance;
+            var heroPos = hero.transform.position;
+            if (heroPos.x >= -100.0f && heroPos.y >= -100.0f)
             {
-                yield return new WaitWhile(() => !HeroController.instance);
-                var hero = HeroController.instance;
-                var heroPos = hero.transform.position;
-                while (heroPos.x >= -100.0f && heroPos.y >= -100.0f)
-                {
-                    heroPos = hero.transform.position;
-                    yield return null;
-                }
-
-                var failSafe = new GameObject("FAILSAFE");
-                failSafe.transform.SetPosition2D(heroPos);
-                var tp = failSafe.AddComponent<TransitionPoint>();
-
-                var bc = failSafe.AddComponent<BoxCollider2D>();
-                bc.size = new Vector2(10f, 10f);
-                bc.isTrigger = true;
-                tp.SetTargetScene("Town");
-                tp.entryPoint = "top1";
-
-                var rm = new GameObject("Hazard Respawn Marker");
-                rm.transform.parent = failSafe.transform;
-                rm.transform.SetPosition2D(new Vector2(0f, 0f));
-                var tmp = rm.AddComponent<HazardRespawnMarker>();
-                tmp.respawnFacingRight = false;
-                tp.respawnMarker = rm.GetComponent<HazardRespawnMarker>();
-                tp.sceneLoadVisualization = GameManager.SceneLoadVisualizations.Default;
+                return;
             }
-            // ReSharper disable once IteratorNeverReturns
+
+            var failSafe = new GameObject("FAILSAFE");
+            failSafe.transform.SetPosition2D(heroPos);
+            var tp = failSafe.AddComponent<TransitionPoint>();
+
+            var bc = failSafe.AddComponent<BoxCollider2D>();
+            bc.size = new Vector2(10f, 10f);
+            bc.isTrigger = true;
+            tp.SetTargetScene("Town");
+            tp.entryPoint = "top1";
+
+            var rm = new GameObject("Hazard Respawn Marker");
+            rm.transform.parent = failSafe.transform;
+            rm.transform.SetPosition2D(new Vector2(0f, 0f));
+            var tmp = rm.AddComponent<HazardRespawnMarker>();
+            tmp.respawnFacingRight = false;
+            tp.respawnMarker = rm.GetComponent<HazardRespawnMarker>();
+            tp.sceneLoadVisualization = GameManager.SceneLoadVisualizations.Default;
         }
     }
 }
