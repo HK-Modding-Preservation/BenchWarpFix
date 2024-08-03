930bfb1226bcf9eb964ff8b66edb8e2c5989c0e7
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-08-03 21:28:40 +0200

    file-scoped namespaces fix csproj

--- src/BenchWarpFix.cs
+++ src/BenchWarpFix.cs
@@ -1,54 +1,52 @@
-﻿using System.Collections;
-using System.Reflection;
+﻿using System.Reflection;
 using Modding;
 using UnityEngine;
 
-namespace BenchWarpFix
+namespace BenchWarpFix;
+
+public class BenchWarpFix : Mod
 {
-    public class BenchWarpFix : Mod
+    internal static BenchWarpFix Instance;
+
+    public override string GetVersion()
     {
-        internal static BenchWarpFix Instance;
+        return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+    }
 
-        public override string GetVersion()
+    public override void Initialize()
+    {
+        Instance = this;
+
+        On.HeroController.FixedUpdate += this.OnHeroControllerFixedUpdate;
+    }
+
+    private void OnHeroControllerFixedUpdate(On.HeroController.orig_FixedUpdate orig, HeroController self)
+    {
+        orig(self);
+
+        var heroPos = self.transform.position;
+
+        if (heroPos.x >= -10000.0f && heroPos.y >= -10000.0f)
         {
-            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            return;
         }
 
-        public override void Initialize()
-        {
-            Instance = this;
+        var failSafe = new GameObject("FAILSAFE");
+        failSafe.transform.SetPosition2D(heroPos);
+        var tp = failSafe.AddComponent<TransitionPoint>();
 
-            On.HeroController.FixedUpdate += this.OnHeroControllerFixedUpdate;
-        }
+        var bc = failSafe.AddComponent<BoxCollider2D>();
+        bc.size = new Vector2(10f, 10f);
+        bc.isTrigger = true;
+        tp.SetTargetScene("Town");
+        tp.entryPoint = "top1";
 
-        private void OnHeroControllerFixedUpdate(On.HeroController.orig_FixedUpdate orig, HeroController self)
-        {
-            orig(self);
-
-            var heroPos = self.transform.position;
-
-            if (heroPos.x >= -10000.0f && heroPos.y >= -10000.0f)
-            {
-                return;
-            }
-
-            var failSafe = new GameObject("FAILSAFE");
-            failSafe.transform.SetPosition2D(heroPos);
-            var tp = failSafe.AddComponent<TransitionPoint>();
-
-            var bc = failSafe.AddComponent<BoxCollider2D>();
-            bc.size = new Vector2(10f, 10f);
-            bc.isTrigger = true;
-            tp.SetTargetScene("Town");
-            tp.entryPoint = "top1";
-
-            var rm = new GameObject("Hazard Respawn Marker");
-            rm.transform.parent = failSafe.transform;
-            rm.transform.SetPosition2D(new Vector2(0f, 0f));
-            var tmp = rm.AddComponent<HazardRespawnMarker>();
-            tmp.respawnFacingRight = false;
-            tp.respawnMarker = rm.GetComponent<HazardRespawnMarker>();
-            tp.sceneLoadVisualization = GameManager.SceneLoadVisualizations.Default;
-        }
+        var rm = new GameObject("Hazard Respawn Marker");
+        rm.transform.parent = failSafe.transform;
+        rm.transform.SetPosition2D(new Vector2(0f, 0f));
+        var tmp = rm.AddComponent<HazardRespawnMarker>();
+        tmp.respawnFacingRight = false;
+        tp.respawnMarker = rm.GetComponent<HazardRespawnMarker>();
+        tp.sceneLoadVisualization = GameManager.SceneLoadVisualizations.Default;
     }
 }
